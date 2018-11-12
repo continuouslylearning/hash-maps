fffb9c310dd355aed1801a6e2655b14b1f3331c0
Timothy <superfluous11@gmail.com>
2018-11-11 17:11:42 -0800

    Created findslot, put and resize methods on hash map

--- hash-map.js
+++ hash-map.js
@@ -17,4 +17,48 @@ class HashMap {
     
   }
 
+  findSlot(key){
+    const hash = HashMap.hash(key);
+    const start = hash % this.capacity;
+    for(let i = start; i < start + this.capacity; i++){
+      const index = i % this.capacity;
+      const slot = this.slots[index];
+      if(!slot || (!slot.deleted && slot.key === key)){
+        return index;
+      }
+    }
+  }
+
+  resize(capacity){
+    const oldSlots = this.slots;
+    this.slots = [];
+    this.capacity = capacity;
+    this.length = 0;
+    oldSlots.forEach(slot => {
+      if(!slot || slot.deleted) return;
+      const { key, value } = slot;
+      this.put(key, value);
+    });
+  }
+
+  put(key, value){
+    if((this.length + 1)/this.capacity > HashMap.LOAD_RATIO) resizeBy(this.capacity * HashMap.SIZE_RATIO);
+    const index = findSlot(key);
+    this.slots[index] = {
+      key,
+      value, 
+      deleted: false
+    };
+  }
+
+  print(){
+    this.slots.forEach(slot => {
+      if(!slot || slot.deleted) return;
+      const { key, value } = slot;
+      console.log(`${key}: ${value}`);
+    });
+  }
 }
+
+HashMap.LOAD_RATIO = .90;
+HashMap.SIZE_RATIO = 3;
